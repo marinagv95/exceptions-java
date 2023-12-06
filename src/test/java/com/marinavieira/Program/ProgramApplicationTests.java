0ddd5462ac0db51e6f4b29dd93e8436213ecd159
marigv95 <mama_guima04@hotmail.com>
2023-12-06 13:52:52 -0300

    First commit

---
+++ src/test/java/com/marinavieira/Program/ProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinavieira.Program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
