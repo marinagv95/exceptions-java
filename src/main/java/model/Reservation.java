1cdc4cc929b67955c80319b5943f1b12495ae805
marigv95 <mama_guima04@hotmail.com>
2023-12-06 14:29:03 -0300

    Very bad solution

---
+++ src/main/java/model/Reservation.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class Reservation {
+    private Integer roomNumber;
+    @Getter
+    private Date checkIn;
+    @Getter
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
